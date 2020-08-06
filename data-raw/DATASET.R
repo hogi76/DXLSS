5056be61f95089d06836044afc91feacaa98170a
AhnDeukho <hogi76@gmail.com>
2020-08-06 22:47:57 +0900

    naming change

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,10 +1,12 @@
 ## code to prepare `DATASET` dataset goes here
 
 library(dplyr);library(tidyr)
-pss4=read.csv('PSS2_rawdata4.csv')
+pss5=read.csv('PSS2_rawdata5.csv')
+colnames(pss5)[2:6]
 
 usethis::use_data(DATASET, overwrite = TRUE)
-usethis::use_data(pss4, compress='xz')
+usethis::use_data(pss5, compress='xz')
 
 
 colnames(pss4)
+pss2 %>% head(2)
