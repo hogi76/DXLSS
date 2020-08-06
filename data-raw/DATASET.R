35f60a5593136609bdd3957811a1ca0017bd0b24
AhnDeukho <hogi76@gmail.com>
2020-08-06 22:32:00 +0900

    .

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,10 +1,10 @@
 ## code to prepare `DATASET` dataset goes here
 
 library(dplyr);library(tidyr)
-
+pss4=read.csv('PSS2_rawdata4.csv')
 
 usethis::use_data(DATASET, overwrite = TRUE)
-usethis::use_data(pss5, compress='xz')
+usethis::use_data(pss4, compress='xz')
 
 
-
+colnames(pss4)
