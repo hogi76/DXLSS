917d84a9cf3ea05174a8e28668b72dcb80f4d136
AhnDeukho <hogi76@gmail.com>
2020-09-18 00:13:59 +0900

    data수정

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,7 +4,7 @@ library(devtools)
 library(dplyr);library(tidyr)
 pss2=read.csv('pss20.csv')
 pss2=pss2 %>% select(-1)
-
+pss2 %>% head(2)
 usethis::use_data(DATASET, overwrite = TRUE)
 usethis::use_data(pss2, compress='xz')
 
